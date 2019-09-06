526b9c718dc048e73838e1ea69b54da18cafcf38
Denis Shatohin <sharoph@live.ru>
2019-09-06 12:31:05 +0300

    Minor improvements

--- mysql-nanoserver_5.7-1809/start.ps1
+++ mysql-nanoserver_5.7-1809/start.ps1
@@ -1,14 +1,31 @@
 ﻿Param(
 [Parameter(Mandatory=$false)]
-[String]$MYSQL_ROOT_PWD
+[String]$MYSQL_ROOT_PWD,
+
+[Parameter(Mandatory=$false)]
+[String]$DB_NAME,
+
+[Parameter(Mandatory=$false)]
+[String]$DB_USER,
+
+[Parameter(Mandatory=$false)]
+[String]$DB_USER_PWD
 )
 
-Write-Verbose "Setting PATH for MySQL Server"
 $env:Path += ";C:\MySQL\bin"
 setx Path $env:Path /m
 
 Write-Verbose "Starting MySQL Server"
 Start-Service MySQL
+
+if(($DB_NAME -ne "_") -and ($DB_USER -ne "_") -and ($DB_USER_PWD -ne "_"))
+{
+    Write-Verbose "Creating database $DB_NAME"
+    $DB_INIT = "CREATE DATABASE" + " $DB_NAME " + "DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" `
+    + "GRANT ALL ON" + " $DB_NAME.* " + "to" + " '$DB_USER'@'%' IDENTIFIED BY" + " '" + $DB_USER_PWD + "';" `
+    + "FLUSH PRIVILEGES;"
+    mysql --user=root --execute="$DB_INIT"
+}
 
 if($MYSQL_ROOT_PWD -eq "_") {
     $secretPath = $env:MYSQL_PWD_PATH
@@ -29,12 +46,4 @@ if($MYSQL_ROOT_PWD -ne "_")
     mysql --user=root --execute="$MYSQL_INIT"
 }
 
-Write-Verbose "Started MySQL Server"
-
-$LastCheck = (Get-Date).AddSeconds(-2) 
-while ($true) 
-{ 
-    Get-WinEvent -FilterHashtable @{ ProviderName='MySQL'; StartTime=$LastCheck } | Select-Object TimeCreated, LevelDisplayName, Message	 
-    $LastCheck = Get-Date 
-    Start-Sleep -Seconds 2 
-}
+C:\ServiceMonitor.exe "mysql"
